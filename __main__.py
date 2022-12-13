3468a1922493f7669b8d31fc9475a0b1441d72d1
Battle-Of-Two-K <tr.penrose@mail.ru>
2022-12-13 21:20:10 +0300

    что-то считает

--- __main__.py
+++ __main__.py
@@ -1,7 +1,5 @@
 from dataclasses import dataclass, field
 from datetime import timedelta
-import matplotlib.pyplot as plt
-
 
 SEPARATOR = ";"
 
@@ -14,48 +12,31 @@ class ConstParams:
     """
 
     gravity: float = 9.81
-    m_alpha_z: float = -.0033
-    m_delta_z: float = -.0025
+    m_alpha_z: float = -0.0033
+    m_delta_z: float = -0.0015
     radius: float = 57.3
     wing_area: float = 5.34
     q: float = 1236.25
     b_a: float = 1.684
     n_x: float = -7.0
     var_part_weight: float = 1.3
-    airplane_wight: float = 132.55
-    airplane_inertia_radius: float = field(default=0, init=False)
-    pipe_inertia_radius: float = field(default=0, init=False)
-
-
-CONST_PARAMS = ConstParams()
-STEP = timedelta(milliseconds=1)
-
-
-def plot(data: dict):
-    abscissa = data["time [сек]"]
-    colour = ["red", "blue", "green", "black", "purple"]
-    params = ["epsilon", "omega", "alpha", "delta_р.в."]
-
-    fig, axs = plt.subplots(len(params))
-
-    for key, item in zip(params, range(len(params))):
-        axs[item].plot(abscissa, data[key], colour[item])
-        axs[item].set_title(key)
-
-    plt.savefig(f"fig.jpg")
-    plt.show()
+    MPADS_weight: float = 50  # MPADS - Man-portable air-defense systems (ПЗРК)
+    airplane_weight: float = 132.55
 
 
 @dataclass
 class Data:
-    airplane_radius_moment_inertia: float = 0
+    rockets_set: list = field(repr=False)
+    time_point: timedelta = timedelta(seconds=0)
+    rocket_launch_counter: int = 0
+    airplane_moment_inertia_radius: float = 0.911529735
     airplane_moment_inertia: float = 0
-    rocket_radius_moment_inertia: float = 0
+    tube_moment_inertia_radius: float = 0.881383571
     rocket_moment_inertia: float = 0
     general_moment_inertia: float = 0
     airplane_acceleration: float = 0
     airplane_velocity: float = 0
-    airplane_attack_angle: float = 0
+    airplane_attack_angle: float = 4.0
     elevator_angle: float = 0
     rocket_velocity: float = 0
     rocket_way: float = 0
@@ -63,8 +44,51 @@ class Data:
     rocket_moment: float = 0
     general_moment: float = 0
 
-    def recalc_data(self):
-        pass
+    def recalc_data(self, time_point: timedelta):
+        self.time_point = time_point
+        self.airplane_moment_inertia_radius = 0.911529735
+        self.airplane_moment_inertia = self.calc_airplane_moment_inertia()
+        self.tube_moment_inertia_radius = 0.881383571
+        self.rocket_moment_inertia = self.calc_rocket_moment_inertia()
+        self.general_moment_inertia = self.airplane_moment_inertia + self.rocket_moment_inertia
+        self.airplane_acceleration = self.calc_airplane_acceleration()
+        self.airplane_velocity = self.calc_airplane_velocity()
+        self.airplane_attack_angle += self.calc_airplane_attack_angle()
+        self.elevator_angle = self.calc_elevator_angle()
+        self.rocket_velocity = 0
+        self.rocket_way = 0
+        self.rocket_power = 0
+        self.rocket_moment = 0
+        self.general_moment = self.calc_general_moment()
+
+    def calc_airplane_moment_inertia(self):
+        return (self.airplane_moment_inertia_radius ** 2) *\
+            (CONST_PARAMS.airplane_weight - self.rocket_launch_counter *
+             (CONST_PARAMS.var_part_weight + self.rockets_set[self.rocket_launch_counter - 1].weight))
+
+    def calc_rocket_moment_inertia(self):
+        return (self.tube_moment_inertia_radius ** 2) * \
+            (CONST_PARAMS.MPADS_weight - self.rocket_launch_counter *
+             (CONST_PARAMS.var_part_weight + self.rockets_set[self.rocket_launch_counter - 1].weight))
+
+    def calc_airplane_acceleration(self):
+        return (57.3 * self.general_moment * CONST_PARAMS.gravity) / self.general_moment_inertia
+
+    def calc_airplane_velocity(self):
+        return self.airplane_acceleration * STEP.total_seconds()
+
+    def calc_airplane_attack_angle(self):
+        return self.airplane_velocity * STEP.total_seconds()
+
+    def calc_elevator_angle(self):
+        return - CONST_PARAMS.m_alpha_z * self.airplane_attack_angle / CONST_PARAMS.m_delta_z
+
+    def calc_general_moment(self):
+        return self.rocket_moment + \
+               self.airplane_attack_angle * CONST_PARAMS.m_alpha_z * (CONST_PARAMS.q * CONST_PARAMS.wing_area *
+                                                                      CONST_PARAMS.b_a / CONST_PARAMS.gravity) + \
+               self.elevator_angle * CONST_PARAMS.m_delta_z * (CONST_PARAMS.q * CONST_PARAMS.wing_area *
+                                                               CONST_PARAMS.b_a / CONST_PARAMS.gravity)
 
 
 @dataclass(repr=True)
@@ -74,52 +98,76 @@ class Rocket:
     time_start: timedelta
     flight_duration: timedelta
     weight: float = 15
-    velocity = 0
-    lp = 0
+
+    def consider_moment(self, cur_data: Data, time: timedelta):
+
+        if time == self.time_start:
+            cur_data.rocket_way = 0
+            cur_data.rocket_velocity = 0
+            cur_data.rocket_way = 0
+            cur_data.airplane_velocity = 0
+        else:
+            cur_data.rocket_velocity += CONST_PARAMS.n_x * CONST_PARAMS.gravity * STEP.total_seconds()
+            cur_data.rocket_way += cur_data.rocket_velocity * STEP.total_seconds()
+
+        cur_data.time_point = time
+        time_diff = time - self.time_start
+        cur_data.tube_moment_inertia_radius = self.coord_y ** 2 + self.coord_x[int(1000 * time_diff.total_seconds())] ** 2
+        cur_data.rocket_moment_inertia = cur_data.tube_moment_inertia_radius * CONST_PARAMS.MPADS_weight
+        cur_data.airplane_moment_inertia = cur_data.calc_airplane_moment_inertia()
+        cur_data.airplane_acceleration = cur_data.calc_airplane_acceleration()
+        cur_data.general_moment_inertia = cur_data.airplane_moment_inertia + cur_data.rocket_moment_inertia
+        cur_data.airplane_velocity += cur_data.calc_airplane_velocity()
+        cur_data.airplane_attack_angle += cur_data.calc_airplane_attack_angle()
+        cur_data.elevator_angle = cur_data.calc_elevator_angle()
+        cur_data.rocket_power = (CONST_PARAMS.var_part_weight + self.weight) * CONST_PARAMS.gravity
+        cur_data.rocket_moment = cur_data.rocket_way * cur_data.rocket_power
+        cur_data.general_moment = cur_data.calc_general_moment()
+
+        if time == self.flight_duration + self.time_start:
+            cur_data.rocket_launch_counter += 1
 
 
 if __name__ == '__main__':
+
+    CONST_PARAMS = ConstParams()
 
     rockets = [
         Rocket(coord_x=[0.055, 0.138, 0.221, 0.304, 0.387, 0.47,
                         0.553, 0.636, 0.719, 0.802, 0.885, 0.968,
                         1.051, 1.134, 1.217, 1.3],
                coord_y=.152,
-               time_start=timedelta(seconds=4),
+               time_start=timedelta(milliseconds=5),
                flight_duration=timedelta(milliseconds=15)),
 
         Rocket(coord_x=[0.075, 0.148, 0.221, 0.294, 0.367, 0.44,
                         0.513, 0.586, 0.659, 0.732, 0.805, 0.878,
                         0.951, 1.024, 1.097, 1.17],
                coord_y=.099,
-               time_start=timedelta(seconds=8),
+               time_start=timedelta(milliseconds=30),
                flight_duration=timedelta(milliseconds=15))
     ]
 
     flight_time = timedelta(seconds=10)
     current = timedelta(milliseconds=0)
+    STEP = timedelta(milliseconds=1)
 
-    calc_data = Data()
+    data = Data(rockets)
+    data.recalc_data(timedelta(seconds=0))
 
     with open("result.csv", "w", encoding="utf-8") as file:
 
-        print(SEPARATOR.join(map(str, calc_data.__dict__.keys())), file=file)
+        print(SEPARATOR.join(map(str, data.__dict__.keys())), file=file)
 
         while current <= flight_time:
-
-            # calc_data.recalc_data(current)
-            #
-            # for rocket in rockets:
-            #     if rocket.time_start <= current <= rocket.time_start + rocket.flight_duration:
-            #         rocket.consider_moment(calc_data, current)
 
             flag = [rocket for rocket in rockets if
                     rocket.time_start <= current <= rocket.flight_duration + rocket.time_start]
 
             if bool(flag) is True:
-                flag[0].consider_moment(calc_data, current)
+                flag[0].consider_moment(data, current)
             else:
-                calc_data.recalc_data(current)
+                data.recalc_data(current)
 
-            print(SEPARATOR.join(map(str, calc_data.__dict__.values())), file=file)
+            print(SEPARATOR.join(map(str, data.__dict__.values())), file=file)
             current = current + STEP
